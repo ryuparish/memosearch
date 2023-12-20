6046ccbc02c4e2c09984603c13801649dba07dc3
Ryu Parish <ryuparish@Ryus-MacBook-Pro-2.local>
2023-12-20 02:24:22 -0800

    Add basic text fields to Links page.
    A few images are added as well.

--- src/boot.sh
+++ src/boot.sh
@@ -1,7 +1,7 @@
 kill -9 $(lsof -t -i:5000)
 echo killed pre
-python engine/app.py &
+python engine/app.py >> backend_log.txt &
 cd gui
-npm start
+npm start >> gui_log.txt
 kill -9 $(lsof -t -i:5000)
 echo killed
